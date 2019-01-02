b4cede00a0ef7cf55c15ee9b765cabf42521797f
rykyou <rkim1214@gmail.com>
2019-01-02 12:55:43 -0500

    fixed guest method and association

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :interest
   has_many :events_guests
-  has_many :users, through: :events_guests
+  has_many :guests, through: :events_guests, foreign_key: 'guest_id'
   belongs_to :host, class_name: 'User'
 end
