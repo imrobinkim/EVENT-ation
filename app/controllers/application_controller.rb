cbc8489f3f2001306046b23f453fb8e4a0d5fd80
github username <github email address>
2019-01-03 10:07:01 -0500

    made helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user, :first_name, :last_name, :no_empty_fields, :events_attending
 
   def current_user #MEMOIZATION
     if @current_user
@@ -8,4 +8,25 @@ class ApplicationController < ActionController::Base
       @current_user = User.find_by(username: cookies[:current_user])
     end
   end
+
+  def first_name(full_name)
+    full_name.split(" ")[0]
+  end
+
+  def last_name(full_name)
+    full_name.split(" ")[-1]
+  end
+
+  def no_empty_fields(event_info)
+      event_info.select do |info|
+        info
+      end
+  end
+
+  def events_attending(person)
+  person.events.find do |x|
+     return x.name
+     end
+  end
+
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,6 @@ class Event < ApplicationRecord
   has_many :events_guests
   has_many :guests, through: :events_guests, foreign_key: 'guest_id'
   belongs_to :host, class_name: 'User'
+
+
 end
